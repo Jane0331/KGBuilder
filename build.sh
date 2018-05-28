87195bcb76b2d0aef0c36c2bae0da120d3be4b69
Jane0331 <jane0331@126.com>
2018-05-28 16:31:46 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ model_re=$4
 output_ner=${input}.entity
 
 #named entity recognition
-python ./NER/tagger.py --modle $model_ner --input $input --output $output_ner
+python ./NER/tagger.py --model $model_ner --input $input --output $output_ner
 #relation extraction
 
 
